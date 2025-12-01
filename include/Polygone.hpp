204759643a7d3fe1d0ad3398f1aef72ed3288632
CAU <simon.cau0@gmail.com>
2025-12-01 15:54:57 +0100

    implementation polygone

--- include/Polygone.hpp
+++ include/Polygone.hpp
@@ -1,38 +1,78 @@
+#pragma once
 #include <iostream>
 #include <vector>
-#pragma once
 #include "Point2D.hpp"
 
 template<typename T>
 class Polygone;
 
 template<typename T>
-std::ostream& operator<<(std::ostream &,Polygone<T> const& p);
+std::ostream& operator<<(std::ostream& os, Polygone<T> const& p);
 
 template<typename T>
 class Polygone {
-    private:
+private:
+    std::vector< Point2D<T> > sommets;
 
-        vector< Point2D<T> > sommets;
+public:
+    Polygone() {}// Constructeur par défaut
+    Polygone(std::vector< Point2D<T> > const& listeSommets)// Constructeur avec liste de sommets
+        : sommets(listeSommets) {}
 
-    public:
-        Polygone(){}// constructeur
-        Polygone(vector< Point2D<T> > const& listeSommets){}
+    // Setteurs et Getters
+    std::vector< Point2D<T> > getSommets() const; 
+    void setSommets(std::vector< Point2D<T> > const& listeSommets);
+    void addSommet(Point2D<T> const& p); 
 
-        // Setteurs et Guetteurs
-        vector< Point2D<T> > getSommets();
-        void setSommets(vector<Point2D<T>> listeSommets);
-        void addSommets(Point2D<T>);
+    // Translation
+    void translate(T x, T y);
 
-        // Translation
-        void translate(T x, T y);
+    virtual void afficher(std::ostream& os) const;
 
-    
-        virtual void afficher(std::ostream& os);
-
-        // Déclaration de l'opérateur << comme fonction amie
-        friend std::ostream& operator<< <T>(std::ostream& os, Polygone<T> const& f);
+    friend std::ostream& operator<< <T>(std::ostream& os, Polygone<T> const& p);
 };
 
+
 template<typename T>
-std::ostream& operator<<(std::ostream& os, Polygone<T> const& f);
+std::vector< Point2D<T> > Polygone<T>::getSommets() const {
+    return this->sommets;
+}
+
+
+template<typename T>
+void Polygone<T>::setSommets(std::vector< Point2D<T> > const& listeSommets) {
+    this->sommets = listeSommets;
+}
+
+
+template<typename T>
+void Polygone<T>::addSommet(Point2D<T> const& p) {
+    this->sommets.push_back(p);
+}
+
+
+template<typename T>
+void Polygone<T>::translate(T x, T y) {
+    for (auto& p : this->sommets) {
+        p.translate(x, y); 
+    }
+}
+
+
+template<typename T>
+void Polygone<T>::afficher(std::ostream& os) const {
+    os << "Polygone [";
+    for (size_t i = 0; i < sommets.size(); ++i) {
+        os << sommets[i]; 
+        if (i < sommets.size() - 1) {
+            os << ", ";
+        }
+    }
+    os << "]";
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, Polygone<T> const& p) {
+    p.afficher(os);
+    return os;
+}
