68c7f33195517c586761d14baf6b21227f95a2fb
CAU <simon.cau0@gmail.com>
2025-12-01 16:02:49 +0100

    Doc Doxygen

--- include/Point2D.hpp
+++ include/Point2D.hpp
@@ -1,44 +1,131 @@
-#include <iostream>
+/**
+ * @file Point2D.hpp
+ * @brief Définition d'une classe template représentant un point en deux dimensions.
+ * @author CAU_Simon
+ * @version 1.0
+ */
+
 #pragma once
+#include <iostream>
+
 
 template <typename T>
 class Point2D;
 
+
 template <typename T>
 std::ostream &operator<<(std::ostream &, Point2D<T> const &p);
 
+/**
+ * @class Point2D
+ * @brief Classe template pour gérer des coordonnées 2D (x, y).
+ * * Cette classe permet de manipuler des points avec des types génériques
+ * (int, float, double, etc.) et offre des fonctionnalités de translation.
+ * * @tparam T Le type de donnée des coordonnées (ex: int, double).
+ */
 template <typename T>
 class Point2D
 {
 protected:
-    T x;
-    T y;
+    T x; /**< Coordonnée sur l'axe des abscisses */
+    T y; /**< Coordonnée sur l'axe des ordonnées */
 
 public:
-    Point2D(T x, T y);         // constructeur avec les deux parametres
-    Point2D(Point2D const &p); // Recopie de p
-    Point2D();                 // constructeur
+    /**
+     * @brief Constructeur avec paramètres.
+     * @param x La coordonnée X initiale.
+     * @param y La coordonnée Y initiale.
+     */
+    Point2D(T x, T y);
 
-    // Setteurs et Guetteurs
+    /**
+     * @brief Constructeur de copie.
+     * @param p Le point à copier.
+     */
+    Point2D(Point2D const &p);
+
+    /**
+     * @brief Constructeur par défaut.
+     * Initialise le point (généralement à 0,0 selon l'implémentation).
+     */
+    Point2D();
+
+    // Setteurs et Getters
+
+    /**
+     * @brief Récupère la coordonnée X.
+     * @return La valeur de x.
+     */
     T getX() const;
+
+    /**
+     * @brief Définit la coordonnée X.
+     * @param x La nouvelle valeur de x.
+     */
     void setX(T x);
+
+    /**
+     * @brief Récupère la coordonnée Y.
+     * @return La valeur de y.
+     */
     T getY() const;
+
+    /**
+     * @brief Définit la coordonnée Y.
+     * @param y La nouvelle valeur de y.
+     */
     void setY(T y);
 
-    // Translation
+    // Méthodes de Translation
+
+    /**
+     * @brief Translate le point horizontalement.
+     * @param a La valeur à ajouter à x.
+     */
     void translateX(T a);
+
+    /**
+     * @brief Translate le point verticalement.
+     * @param b La valeur à ajouter à y.
+     */
     void translateY(T b);
+
+    /**
+     * @brief Translate le point sur les deux axes.
+     * @param a La valeur à ajouter à x.
+     * @param b La valeur à ajouter à y.
+     */
     void translate(T a, T b);
 
-    // opérateur d'affichage
-    friend std::ostream &operator<< <T>(std::ostream &s, Point<T> const &p);
+    /**
+     * @brief Surcharge de l'opérateur de flux pour l'affichage.
+     * @param s Le flux de sortie (ex: std::cout).
+     * @param p Le point à afficher.
+     * @return Une référence vers le flux de sortie.
+     */
+    friend std::ostream &operator<< <T>(std::ostream &s, Point2D<T> const &p);
 };
+
+
+// Implémentation des méthodes
+
+template <typename T>
+Point2D<T>::Point2D() : x(0), y(0) 
+{
+    // Initialisation par défaut à 0
+}
 
 template <typename T>
 Point2D<T>::Point2D(T x, T y)
 {
     this->x = x;
     this->y = y;
+}
+
+template <typename T>
+Point2D<T>::Point2D(Point2D<T> const &p) : x(p.x), y(p.y) 
+{
+    // Copie effectuée via la liste d'initialisation
 }
 
 template <typename T>
@@ -90,6 +177,3 @@ std::ostream &operator<<(std::ostream &o, Point2D<T> const &p)
     o << "x = " << p.getX() << ", y = " << p.getY();
     return o;
 }
-
-template <typename T>
-Point2D<T>::Point2D(Point2D<T> const &p) : x(p.x), y(p.y) {}
--- include/Polygone.hpp
+++ include/Polygone.hpp
@@ -1,69 +1,127 @@
+/**
+ * @file Polygone.hpp
+ * @brief Définition d'une classe template représentant un polygone géométrique.
+ * @author CAU_Simon
+ * @version 1.0
+ */
+
 #pragma once
 #include <iostream>
 #include <vector>
 #include "Point2D.hpp"
 
+
 template<typename T>
 class Polygone;
+
 
 template<typename T>
 std::ostream& operator<<(std::ostream& os, Polygone<T> const& p);
 
+/**
+ * @class Polygone
+ * @brief Classe template gérant une collection de points (sommets) formant un polygone.
+ * * Cette classe stocke une liste de Point2D et permet d'effectuer des opérations 
+ * globales comme la translation ou l'affichage de la forme complète.
+ * * @tparam T Le type de donnée des coordonnées des sommets (ex: int, double).
+ */
 template<typename T>
 class Polygone {
 private:
+    /** * @brief Vecteur contenant la liste ordonnée des sommets du polygone. 
+     */
     std::vector< Point2D<T> > sommets;
 
 public:
-    Polygone() {}// Constructeur par défaut
-    Polygone(std::vector< Point2D<T> > const& listeSommets)// Constructeur avec liste de sommets
+    /**
+     * @brief Constructeur par défaut.
+     * Crée un polygone vide (sans sommets).
+     */
+    Polygone() {} 
+
+    /**
+     * @brief Constructeur avec initialisation.
+     * @param listeSommets Un vecteur de Point2D pour initialiser le polygone.
+     */
+    Polygone(std::vector< Point2D<T> > const& listeSommets)
         : sommets(listeSommets) {}
 
     // Setteurs et Getters
+
+    /**
+     * @brief Récupère la liste des sommets.
+     * @return Un vecteur contenant tous les points du polygone.
+     */
     std::vector< Point2D<T> > getSommets() const; 
+
+    /**
+     * @brief Remplace la liste actuelle des sommets.
+     * @param listeSommets Le nouveau vecteur de points.
+     */
     void setSommets(std::vector< Point2D<T> > const& listeSommets);
+
+    /**
+     * @brief Ajoute un nouveau sommet à la fin de la liste du polygone.
+     * @param p Le point à ajouter.
+     */
     void addSommet(Point2D<T> const& p); 
 
-    // Translation
+    // Opérations Géométriques
+
+    /**
+     * @brief Translate le polygone entier.
+     * Applique une translation (x, y) à chaque sommet du polygone.
+     * @param x Valeur de déplacement horizontal.
+     * @param y Valeur de déplacement vertical.
+     */
     void translate(T x, T y);
 
+    /**
+     * @brief Affiche le contenu du polygone dans un flux.
+     * Format de sortie : Polygone [Point1, Point2, ...]
+     * @param os Le flux de sortie.
+     */
     virtual void afficher(std::ostream& os) const;
 
+    /**
+     * @brief Surcharge de l'opérateur de flux pour l'affichage standard.
+     * @param os Le flux de sortie (ex: std::cout).
+     * @param p Le polygone à afficher.
+     * @return Une référence vers le flux de sortie.
+     */
     friend std::ostream& operator<< <T>(std::ostream& os, Polygone<T> const& p);
 };
 
+// Implémentation des méthodes
 
 template<typename T>
 std::vector< Point2D<T> > Polygone<T>::getSommets() const {
     return this->sommets;
 }
 
-
 template<typename T>
 void Polygone<T>::setSommets(std::vector< Point2D<T> > const& listeSommets) {
     this->sommets = listeSommets;
 }
-
 
 template<typename T>
 void Polygone<T>::addSommet(Point2D<T> const& p) {
     this->sommets.push_back(p);
 }
 
-
 template<typename T>
 void Polygone<T>::translate(T x, T y) {
+    // Itère sur chaque point par référence pour le modifier
     for (auto& p : this->sommets) {
         p.translate(x, y); 
     }
 }
 
-
 template<typename T>
 void Polygone<T>::afficher(std::ostream& os) const {
     os << "Polygone [";
     for (size_t i = 0; i < sommets.size(); ++i) {
-        os << sommets[i]; 
+        os << sommets[i]; // Utilise l'opérateur << de Point2D
         if (i < sommets.size() - 1) {
             os << ", ";
         }
