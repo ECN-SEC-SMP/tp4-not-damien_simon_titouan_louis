7a4c278baaede9fc696bdb605c2dbc2999d28e4e
Titouan Copin <titouan.copin@outlook.fr>
2025-12-01 18:00:53 +0100

    Fix Constructible

--- include/Constructible.hpp
+++ include/Constructible.hpp
@@ -1,7 +1,14 @@
 #pragma once
+
+#include <string>
+
+#include "Polygone.hpp"
 #include "Parcelle.hpp"
 
 class Constructible: public Parcelle {
     public:
-        virtual float getSurfaceConstructible() = 0;
+        Constructible(int num, std::string prop, Polygone<float> forme);
+        Constructible(Parcelle parc);
+        ~Constructible();
+        virtual float getSurfaceConstructible() const = 0;
 };
