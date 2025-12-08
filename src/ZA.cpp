3174cd37aeaf2aec1d6d890a7d22cb2ca3658587
Louis Vaillant <louis.vaillant@eleves.ec-nantes.fr>
2025-12-08 17:08:14 +0100

    [Fix] (ZA) Constructor fixed

--- src/ZA.cpp
+++ src/ZA.cpp
@@ -1,7 +1,7 @@
 #include "ZA.hpp"
 
 ZA::ZA(int num, std::string prop, Polygone<float> forme, TypeCulture typeCulture, int pourcentageConstructible)
-    : ZN(num, prop, forme), Constructible(num, prop, forme), typeCulture(typeCulture), Parcelle(num, prop, forme)
+    : Parcelle(num, prop, forme), ZN(num, prop, forme), Constructible(num, prop, forme), typeCulture(typeCulture)
 {
     this->type = "Zone Agricole";
     this->pourcentageConstructible = pourcentageConstructible;
