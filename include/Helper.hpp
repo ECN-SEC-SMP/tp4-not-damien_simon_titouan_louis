68af6bde1a80dc10cef184c95a8963b4772588c2
Louis Vaillant <louis.vaillant@eleves.ec-nantes.fr>
2025-12-10 17:14:32 +0100

    [Fix] (Helper) Add inline specifier for split() function

--- include/Helper.hpp
+++ include/Helper.hpp
@@ -5,11 +5,13 @@
 
 namespace Helper
 {
-    std::vector<std::string> split(std::string s, const std::string& delimiter) {
+    inline std::vector<std::string> split(std::string s, const std::string &delimiter)
+    {
         std::vector<std::string> tokens;
         size_t pos = 0;
         std::string token;
-        while ((pos = s.find(delimiter)) != std::string::npos) {
+        while ((pos = s.find(delimiter)) != std::string::npos)
+        {
             token = s.substr(0, pos);
             tokens.push_back(token);
             s.erase(0, pos + delimiter.length());
